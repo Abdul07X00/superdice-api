bf635ddb68eb07fcd7c94f5b808774cbdcfc0ccb
ishancs <ihshaan10@gmail.com>
2022-05-29 22:33:25 +0400

    Bug Fix

--- application/models/Wallet_model.php
+++ application/models/Wallet_model.php
@@ -5,7 +5,7 @@
         {
             $wallets = [];
             if($wallet_address){
-                $wallets = $this->db->select('*')->from("tbl_wallet")->where(array("network"=>$network, "status"=>1))->get()->result();
+                $wallets = $this->db->select('*')->from("tbl_wallets")->where(array("network"=>$network, "status"=>1))->get()->result();
                 foreach($wallets as $wallet){
                     $wallet->value_options = json_decode($wallet->value_options);
                     $wallet->value = $this->db->select('*')->from("tbl_transactions")->where(array("wallet_address"=>$wallet_address, "network"=>$wallet->network, "currency"=>$wallet->currency))->order_by("created_at","desc")->get()->row();
